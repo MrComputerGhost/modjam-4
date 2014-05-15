fe92581d01297602ae72e89e62fa5a5d21b53591
markhuang13 <markhuang13@live.com>
2014-05-16 08:45:52 +1000

    Working on the Timer Block

--- src/main/java/mods/elevator/blocks/BlockTimer.java
+++ src/main/java/mods/elevator/blocks/BlockTimer.java
@@ -2,13 +2,22 @@ package mods.elevator.blocks;
 
 import java.util.Random;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+
+import mods.elevator.init.Blocks;
+import mods.elevator.tileentitys.TileEntityTimer;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class BlockTimer extends Block
+public class BlockTimer extends BlockContainer
 {
+	
+	public boolean redstone = false;
 
 	public BlockTimer(Material material)
 	{
@@ -32,16 +41,23 @@ public class BlockTimer extends Block
 
 	public int isProvidingWeakPower(IBlockAccess iBlockAccess, int x, int y, int z, int side)
 	{
-	}
-
-	public int isProvidingStrongPower(IBlockAccess iBlockAccess, int x, int y, int z, int side)
-	{
-		return 0;
+		return redstone;
 	}
 
 	public void updateTick(World world, int x, int y, int z, Random random)
 	{
-		System.out.println("Test");
+	}
+
+	public int tickRate(World world)
+	{
+		return 1;
+	}
+	
+	//Testing
+	public boolean onBlockActivated(World p_149727_1_, int p_149727_2_, int p_149727_3_, int p_149727_4_, EntityPlayer p_149727_5_, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
+	{
+		update(p_149727_1_, p_149727_2_, p_149727_3_, p_149727_4_);
+		return true;
 	}
 
 	public void onBlockAdded(World world, int x, int y, int z)
@@ -52,6 +68,11 @@ public class BlockTimer extends Block
 	public boolean canProvidePower()
 	{
 		return true;
+	}
+
+	public TileEntity createNewTileEntity(World world, int arg1)
+	{
+		return new TileEntityTimer();
 	}
 
 
---
+++ src/main/java/mods/elevator/tileentitys/TileEntityTimer.java
@@ -0,0 +1,14 @@
+package mods.elevator.tileentitys;
+
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityTimer extends TileEntity
+{
+
+	public void updateEntity()
+	{
+	}
+	
+	
+	
+}
