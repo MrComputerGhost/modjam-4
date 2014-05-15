6a932ad6b13850d607ebd35dcf96de7d57884060
MrComputerGhost <MrComputerGhost@users.noreply.github.com>
2014-05-15 16:42:34 -0700

    See if this helps any.

--- src/main/java/mods/elevator/blocks/BlockElevator.java
+++ src/main/java/mods/elevator/blocks/BlockElevator.java
@@ -20,5 +20,14 @@ public class BlockElevator extends BlockContainer
 	{
 		return new TileEntityElevator();
 	}
+	
+	public void breakBlock(World world, int i, int j, int k, int par5, int par6) {
+		TileEntityElevator te = (TileEntityElevator) world.getBlockTileEntity(i, j, k);
+		if (te != null) {
+			world.markTileEntityForDespawn(te);
+		}
+		super.breakBlock(world, i, j, k, par5, par6);
+	}
+
 
 }
